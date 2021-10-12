488c57c36e0728f629a1b71f766269aa8741e479
Homie <ghals0708@gmail.com>
2021-10-13 00:21:13 +0900

    Create 11727.cpp

---
+++ DP/11727.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define MAX 1002
+#define mod 10007
+
+using namespace std;
+int dp[MAX];
+int main() {
+	int n;
+	dp[0] = 1;
+	dp[1] = 3;
+
+	cin >> n;
+
+	for (int i = 2; i < n; i++) {
+		dp[i] = (dp[i - 1] + (2*dp[i - 2])) % mod;
+	}
+	int i;
+	cout << dp[n - 1] << '\n';
+}
