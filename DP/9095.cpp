fbeb13f08d21927cc983ab2ff44212cd50017278
HomieKim <ghals0708@gmail.com>
2021-12-24 16:37:17 +0900

    Create 9095.cpp

---
+++ DP/9095.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[12];
+
+int main() {
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 4; i <= 12; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
+	int T,n;
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		cout << dp[n] << '\n';
+	}
+
+}
