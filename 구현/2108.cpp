aaa63a4ba3c3498dd7eb1bcf2af9078bff335e3c
HomieKim <ghals0708@gmail.com>
2022-04-15 22:02:58 +0900

    Update 2108.cpp

--- 구현/2108.cpp
+++ 구현/2108.cpp
@@ -2,39 +2,22 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+
 using namespace std;
-int arr[8002] = { 0, };
-int main() {
-	int n;
-	cin >> n;
 
-	vector<int> vec;
-	int num;
-	int sum = 0;
+int cntArr[8002] = { 0, };
+vector<int> vec;
 
-	for (int i = 0; i < n; i++) {
-		cin >> num;
-		sum += num;
-		vec.push_back(num);
-		if (num < 0) {
-			arr[num * -1]++;
-		}
-		else {
-			arr[num + 4001]++;
-		}
-
-	}
-
-	sort(vec.begin(), vec.end());
-	int maxEl = *max_element(arr, arr + 8002);
+// ÃÖºó°ª ±¸ÇÏ´Â ÇÔ¼ö
+int getMode(int maxEl) {
+	int rst = 0;
 	vector<int> tmpVec;
 	for (int i = 0; i < 8002; i++) {
-		if (maxEl == arr[i]) {
+		if (maxEl == cntArr[i]) {
 			tmpVec.push_back(i);
 		}
 	}
 
-	int rst = 0;
 	if (tmpVec.size() > 1) {
 		for (int i = 0; i < tmpVec.size(); i++) {
 			if (tmpVec[i] >= 4001) {
@@ -56,6 +39,34 @@ int main() {
 			rst *= -1;
 		}
 	}
+	return rst;
+};
+
+int main() {
+	int n;
+	cin >> n;
+
+	
+	int num;
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		cin >> num;
+		sum += num;
+		vec.push_back(num);
+		if (num < 0) {
+			cntArr[num * -1]++;
+		}
+		else {
+			cntArr[num + 4001]++;
+		}
+
+	}
+
+	sort(vec.begin(), vec.end());
+	
+	int mode = getMode(*max_element(cntArr, cntArr + 8002));
+	
 	// »ê¼úÆò±Õ
 	double avg = round(sum / (double)n);
 	if (avg == -0) {
@@ -65,7 +76,7 @@ int main() {
 	// Áß¾Ó°ª
 	cout << vec[(n / 2 + 1) - 1] << '\n';
 	// ÃÖºó°ª
-	cout << rst << '\n';
+	cout << mode << '\n';
 	// ¹üÀ§
 	cout << vec[n - 1] - vec[0] << '\n';
 
